ea64284dc63f2d0e61764b862d84413269029812
Gemstone5-rx <pearludoka2000@gmail.com>
2023-06-06 11:52:48 -0700

    Added files

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in str:
+        if(ord('a') <= ord(i) <= ord('z')):
+            new_str += (chr(ord(i) - 32))
+        else:
+            new_str += i
+    print(new_str)
